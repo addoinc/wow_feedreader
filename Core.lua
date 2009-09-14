4ac29d5a77fb6b860f3b01556072f78a79e0b6db
Gautam Chekuri <gautam.chekuri@azri.biz>
2009-09-14 20:18:40 +0530

    ensuring that the saved variable is loaded up

--- Core.lua
+++ Core.lua
@@ -1,14 +1,27 @@
-Zugslist = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0", "AceEvent-2.0", "AceDB-2.0")
+Zugslist = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0")
 
+local debug = 0
 function Zugslist:OnInitialize()
 end
 
 function Zugslist:OnEnable()
-	ZLData = {}
 	self:RegisterEvent("RequestUpdates")
+	-- Ensure that the saved variables are loaded
+	self:RegisterEvent("ADDON_LOADED")
+end
+
+function Zugslist:ADDON_LOADED()
+	if(debug == 1) then
+		print("ZUGSLIST LOADED: ", type(ZLData))
+	end
 end
 
 function Zugslist:RequestUpdates()
+	if( debug == 1 and ZLData ~= nil ) then
+		for k,v in pairs(ZLData) do
+			print(k)
+		end
+	end
 	-- Tigger the UpdateTradelinks event of GYP and pass the new data
 	GnomishYellowPages:TriggerEvent("UpdateTradelinks", ZLData)
 end
