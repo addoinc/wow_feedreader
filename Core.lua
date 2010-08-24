03dc87d8c63ea7ff96f34d09895cf8acc8fa7f7a
wow.player <wow.player96@yahoo.in>
2010-08-24 17:52:49 +0530

    fixed typos

--- Core.lua
+++ Core.lua
@@ -1,16 +1,16 @@
-Zugslist = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0")
+wow_feedreader = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0")
 
 local debug = 0
-function Zugslist:OnInitialize()
+function wow_feedreader:OnInitialize()
 end
 
-function Zugslist:OnEnable()
+function wow_feedreader:OnEnable()
    self:RegisterEvent("RequestUpdates")
    -- Ensure that the saved variables are loaded
    self:RegisterEvent("ADDON_LOADED")
 end
 
-function Zugslist:ADDON_LOADED()
+function wow_feedreader:ADDON_LOADED()
    if(debug == 1) then
       print("ZUGSLIST LOADED: ", type(ZLData))
       
@@ -39,7 +39,7 @@ function Zugslist:ADDON_LOADED()
    end
 end
 
-function Zugslist:RequestUpdates()
+function wow_feedreader:RequestUpdates()
    -- Tigger the UpdateTradelinks event of GYP and pass the new data
    GnomishYellowPages:TriggerEvent("UpdateTradelinks", ZLData)
 end
